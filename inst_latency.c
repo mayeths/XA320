cd48016fb79fad0bcebb9e497b11e988a9ce6138
Mayeths <wow@mayeths.com>
2020-08-13 21:54:47 +0800

    Refactor inst_latency but met some bug

--- inst_latency.c
+++ inst_latency.c
@@ -4,70 +4,46 @@
 #include <stdlib.h>
 #include "counter.h"
 
-#define TIMES 1000000
-#define x1(op) op " %0, %0, %1;"
-#define x5(op) x1(op) x1(op) x1(op) x1(op) x1(op)
-#define x10(op) x5(op) x5(op)
-#define x50(op) x10(op) x10(op) x10(op) x10(op) x10(op)
-#define x100(op) x50(op) x50(op)
-#define x500(op) x100(op) x100(op) x100(op) x100(op) x100(op)
-#define x1000(op) x500(op) x500(op)
-#define TEST_INST(op)                                                \
-  {                                                                  \
-    register int a_, b_;                                             \
-    register u_int64_t counter_, i_;                                 \
-    TIK(counter_);                                                   \
-    for (i_ = 0; i_ < TIMES; i_ += 1000) {                           \
-      asm volatile(x1000(op) : "=r"(a_) : "r"(b_));                  \
-    }                                                                \
-    TOK(counter_);                                                   \
-    u_int64_t ns_per_tick_ = 1e9 / counter_freq();                   \
-    u_int64_t total_ns_ = counter_ * ns_per_tick_;                   \
-    double ns_per_inst_ = (double)total_ns_ / TIMES;                 \
-    double cpi_ = ns_per_inst_ / cpu_clock();                        \
-    u_int64_t latency_ = (int)(cpi_ + 0.5);                          \
-    printf("%5s %ld cycles(%.2f ns)\n", op, latency_, ns_per_inst_); \
+#define REPEATS 1000000
+#define X5(inst) inst inst inst inst inst
+#define X10(inst) X5(inst) X5(inst)
+#define X50(inst) X10(inst) X10(inst) X10(inst) X10(inst) X10(inst)
+#define X100(inst) X50(inst) X50(inst)
+#define X500(inst) X100(inst) X100(inst) X100(inst) X100(inst) X100(inst)
+#define X1000(inst) X500(inst) X500(inst)
+
+#define TEST_CORE(op, inst)                               \
+  {                                                       \
+    register int a, b, c;                                 \
+    register u_int64_t counter, i;                        \
+    TIK(counter);                                         \
+    for (i = 0; i < REPEATS; i += 1000) {                 \
+      X1000(inst);                                        \
+    }                                                     \
+    TOK(counter);                                         \
+    u_int64_t ns_per_tick = 1e9 / counter_freq();         \
+    u_int64_t total_ns = counter * ns_per_tick;           \
+    double ns = (double)total_ns / REPEATS;               \
+    double cpi = ns / cpu_clock();                        \
+    u_int64_t latency = (int)(cpi + 0.5);                 \
+    printf("%5s %ld cycles(%.2f ns)\n", op, latency, ns); \
   }
 
-#define X1(op) op " %0, %0, %1, %2;"
-#define X5(op) X1(op) X1(op) X1(op) X1(op) X1(op)
-#define X10(op) X5(op) X5(op)
-#define X50(op) X10(op) X10(op) X10(op) X10(op) X10(op)
-#define X100(op) X50(op) X50(op)
-#define X500(op) X100(op) X100(op) X100(op) X100(op) X100(op)
-#define X1000(op) X500(op) X500(op)
-#define TEST_INST_2(op)                                              \
-  {                                                                  \
-    register int a_, b_, c_;                                         \
-    register u_int64_t counter_, i_;                                 \
-    TIK(counter_);                                                   \
-    for (i_ = 0; i_ < TIMES; i_ += 1000) {                           \
-      asm volatile(X1000(op) : "=r"(a_) : "r"(b_), "r"(c_));         \
-    }                                                                \
-    TOK(counter_);                                                   \
-    u_int64_t ns_per_tick_ = 1e9 / counter_freq();                   \
-    u_int64_t total_ns_ = counter_ * ns_per_tick_;                   \
-    double ns_per_inst_ = (double)total_ns_ / TIMES;                 \
-    double cpi_ = ns_per_inst_ / cpu_clock();                        \
-    u_int64_t latency_ = (int)(cpi_ + 0.5);                          \
-    printf("%5s %ld cycles(%.2f ns)\n", op, latency_, ns_per_inst_); \
-  }
+#define TEST_2(op) TEST_CORE(op, asm volatile(op " %0, %0, %1;" : "=r"(a) : "r"(b));)
+#define TEST_3(op) TEST_CORE(op, asm volatile(op " %0, %0, %1, %2;" : "=r"(a) : "r"(b), "r"(c));)
 
-int main(void) {
+int main() {
   display_counter_info();
-  register int a, b, c;
-  register u_int64_t counter, i;
-  TEST_INST("ADD");
-  TEST_INST("SUB");
-  TEST_INST("MUL");
-  TEST_INST("SDIV");
-  TEST_INST("AND");
-  TEST_INST("EOR");
-  TEST_INST("ORR");
-  TEST_INST("ASR");
-  TEST_INST("LSL");
-  TEST_INST("LSR");
-  TEST_INST("ROR");
-  TEST_INST_2("MADD");
+  TEST_2("ADD");
+  TEST_2("SUB");
+  TEST_2("MUL");
+  TEST_2("SDIV");
+  TEST_2("AND");
+  TEST_2("EOR");
+  TEST_2("ORR");
+  TEST_2("ASR");
+  //FIXME: if insert more test here like TEST_2("LSL"),
+  // ADD and SUB above will become 2 cycles. Confusing.
+  TEST_3("MADD");
   return 0;
 }
