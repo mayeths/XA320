a54877aa3575577e1e5add8943056b8598346c5f
Mayeths <wow@mayeths.com>
2020-08-14 20:12:35 +0800

    Move counter.h to commons.h and refactor

---
+++ commons.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// -------------------- UNROLL ---------------------- //
+
+#define X5(inst) inst inst inst inst inst
+#define X10(inst) X5(inst) X5(inst)
+#define X50(inst) X10(inst) X10(inst) X10(inst) X10(inst) X10(inst)
+#define X100(inst) X50(inst) X50(inst)
+#define X500(inst) X100(inst) X100(inst) X100(inst) X100(inst) X100(inst)
+#define X1000(inst) X500(inst) X500(inst)
+#define INST_2(op, r0, r1) asm volatile(op " %0, %0, %1;" : "=r"(r0) : "r"(r1));
+#define INST_3(op, r0, r1, r2) asm volatile(op " %0, %0, %1, %2;" : "=r"(r0) : "r"(r1), "r"(r2));
+
+// -------------------- COUNTER ---------------------- //
+
+#define GET_SYS_REG(v, REG_NAME) \
+  ({                             \
+    asm volatile(                \
+        "isb;"                   \
+        "mrs %0, " REG_NAME ";"  \
+        : "=r"(v));              \
+  })
+#define ARM_COUNTER(v) GET_SYS_REG(v, "CNTVCT_EL0")
+#define ARM_COUTNER_FREQUENCY(v) GET_SYS_REG(v, "CNTFRQ_EL0")
+#define TICK(v) ARM_COUNTER(v);
+#define TOCK(v) { u_int64_t t_; ARM_COUNTER(t_); v = t_ - v; }
+
+u_int64_t counter_ns() {
+  u_int64_t t;
+  ARM_COUTNER_FREQUENCY(t);
+  return 1e9 / t;
+}
+
+void display_counter_info() {
+  u_int32_t ns = counter_ns();
+  u_int32_t freq = 1e9 / ns;
+  u_int32_t mhz = freq / 1e6;
+  printf(
+    "* System counter info (read from CNTFRQ_EL0): "
+    "%uMHz, %uns, %uticks\n", mhz, ns, freq);
+}
+
+// -------------------- CPU CLOCK ---------------------- //
+// We think each ADD instruction take 1 cycle
+// because the following result is about 0.385ns(2.6GHz)
+// if we run on Huawei Kunpeng 920.
+double cpu_clock() {
+  const int repeats = 1000000;
+  register int a, b;
+  register u_int64_t counter, i;
+  TICK(counter);
+  for (i = 0; i < repeats; i += 1000) {
+    X1000(INST_2("add", a, b))
+  }
+  TOCK(counter);
+  u_int64_t total_ns = counter * counter_ns();
+  double ns_per_inst = (double)total_ns / repeats;
+  return ns_per_inst;
+}
--- inst_latency.c
+++ inst_latency.c
@@ -2,35 +2,28 @@
 // Usage: ./inst_latency
 #include <stdio.h>
 #include <stdlib.h>
-#include "counter.h"
+#include "commons.h"
 
 #define REPEATS 1000000
-#define X5(inst) inst inst inst inst inst
-#define X10(inst) X5(inst) X5(inst)
-#define X50(inst) X10(inst) X10(inst) X10(inst) X10(inst) X10(inst)
-#define X100(inst) X50(inst) X50(inst)
-#define X500(inst) X100(inst) X100(inst) X100(inst) X100(inst) X100(inst)
-#define X1000(inst) X500(inst) X500(inst)
 
 #define TEST_CORE(op, inst)                               \
   {                                                       \
     register int a, b, c;                                 \
     register u_int64_t counter, i;                        \
-    TIK(counter);                                         \
+    TICK(counter);                                        \
     for (i = 0; i < REPEATS; i += 1000) {                 \
       X1000(inst);                                        \
     }                                                     \
-    TOK(counter);                                         \
-    u_int64_t ns_per_tick = 1e9 / counter_freq();         \
-    u_int64_t total_ns = counter * ns_per_tick;           \
+    TOCK(counter);                                        \
+    u_int64_t total_ns = counter * counter_ns();          \
     double ns = (double)total_ns / REPEATS;               \
     double cpi = ns / cpu_clock();                        \
     u_int64_t latency = (int)(cpi + 0.5);                 \
     printf("%5s %ld cycles(%.2f ns)\n", op, latency, ns); \
   }
 
-#define TEST_2(op) TEST_CORE(op, asm volatile(op " %0, %0, %1;" : "=r"(a) : "r"(b));)
-#define TEST_3(op) TEST_CORE(op, asm volatile(op " %0, %0, %1, %2;" : "=r"(a) : "r"(b), "r"(c));)
+#define TEST_2(op) TEST_CORE(op, INST_2(op, a, b))
+#define TEST_3(op) TEST_CORE(op, INST_3(op, a, b, c))
 
 int main() {
   display_counter_info();
