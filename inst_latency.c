50d663562cc513f9ad725b0bc4cb26ead857b37d
Mayeths <wow@mayeths.com>
2020-08-17 11:50:41 +0800

    Avoid gcc optimization bug and add floating point test

--- inst_latency.c
+++ inst_latency.c
@@ -8,8 +8,6 @@
 
 #define TEST_CORE(op, inst)                               \
   {                                                       \
-    register int a, b, c;                                 \
-    register u_int64_t counter, i;                        \
     TICK(counter);                                        \
     for (i = 0; i < REPEATS; i += 1000) {                 \
       X1000(inst);                                        \
@@ -22,11 +20,33 @@
     printf("%5s %ld cycles(%.2f ns)\n", op, latency, ns); \
   }
 
-#define TEST_2(op) TEST_CORE(op, INST_2(op, a, b))
-#define TEST_3(op) TEST_CORE(op, INST_3(op, a, b, c))
+#define TEST_FCORE(op, inst)                              \
+  {                                                       \
+    asm volatile("fmov d0, %0;" :: "r"(da));              \
+    asm volatile("fmov d1, %0;" :: "r"(db));              \
+    asm volatile("fmov d2, %0;" :: "r"(dc));              \
+    TICK(counter);                                        \
+    for (i = 0; i < REPEATS; i += 1000) {                 \
+      X1000(asm volatile(op" d0, d0, d1"););              \
+    }                                                     \
+    TOCK(counter);                                        \
+    u_int64_t total_ns = counter * counter_ns();          \
+    double ns = (double)total_ns / REPEATS;               \
+    double cpi = ns / cpu_clock();                        \
+    u_int64_t latency = (int)(cpi + 0.5);                 \
+    printf("%5s %ld cycles(%.2f ns)\n", op, latency, ns); \
+  }
+
+#define TEST_2(op) TEST_CORE(op, asm volatile(op " %0, %0, %1;" : "=r"(a) : "r"(b));)
+#define TEST_3(op) TEST_CORE(op, asm volatile(op " %0, %0, %1, %2;" : "=r"(a) : "r"(b), "r"(c));)
+#define TEST_F2(op) TEST_FCORE(op, asm volatile(op" d0, d0, d1");)
+#define TEST_F3(op) TEST_FCORE(op, asm volatile(op" d0, d0, d1, d2");)
 
 int main() {
   display_counter_info();
+  register int a, b, c;
+  register double da, db, dc;
+  register u_int64_t counter, i;
   TEST_2("ADD");
   TEST_2("SUB");
   TEST_2("MUL");
@@ -35,8 +55,13 @@ int main() {
   TEST_2("EOR");
   TEST_2("ORR");
   TEST_2("ASR");
-  //FIXME: if insert more test here like TEST_2("LSL"),
-  // ADD and SUB above will become 2 cycles. Confusing.
+  TEST_2("LSL");
+  TEST_2("LSR");
+  TEST_2("ROR");
   TEST_3("MADD");
+  TEST_F2("FADD");
+  TEST_F2("FSUB");
+  TEST_F2("FMUL");
+  TEST_F2("FDIV");
   return 0;
 }
